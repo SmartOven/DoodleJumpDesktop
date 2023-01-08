ed2b4b5b0bf883bd3515d8903123dd404c9c3ce3
smartoven <yarik.panteleev@gmail.com>
2023-01-08 17:52:34 +0300

    Small refactoring x2

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 
-import pygame.time
 from pygame.locals import *
 
 from src.parameters import fps
-from src.platform import Platform
+from src.platform import Platforms
 from src.player import Player
 from src.render_util import *
 from src.util import FramesCounter
@@ -12,22 +11,14 @@ from src.util import FramesCounter
 pygame.init()
 pygame.display.set_caption("Doodle Jump")
 
-platforms = [
-    Platform(Pair(Platform.min_x, Platform.min_y)),
-    Platform(Pair(Platform.min_x, Platform.max_y)),
-    Platform(Pair(Platform.max_x, Platform.min_y)),
-    Platform(Pair(Platform.max_x, Platform.max_y))
-]
-
 if __name__ == "__main__":
     # Game loop
     while True:
         # Rendering
         render_background()
-        Player.render()
 
-        for platform in platforms:
-            platform.render()
+        Platforms.render()
+        Player.render()
 
         # Events catching
         for event in pygame.event.get():
@@ -35,6 +26,7 @@ if __name__ == "__main__":
                 pygame.quit()
                 sys.exit()
 
+            # Moving player
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                     Player.is_moving_left_now()
--- src/platform.py
+++ src/platform.py
@@ -2,6 +2,7 @@ import random
 
 import pygame
 
+from src.display import display_size
 from src.render_util import half_display_size, Pair, at_bottom_with_x, render
 
 
@@ -9,7 +10,6 @@ class Platform:
     image = pygame.image.load("resources/platform.png")
     size = Pair(94, 22)
     half_size = Pair(size.x // 2, size.y // 2)
-    coords = at_bottom_with_x(size, 0)
 
     min_x = -half_display_size.x + half_size.x
     max_x = half_display_size.x - half_size.x
@@ -19,13 +19,45 @@ class Platform:
     def __init__(self, coords: Pair):
         self.coords = coords
 
-    @staticmethod
-    def set_coords(coords: Pair):
-        Platform.coords = coords
-
     def render(self):
         render(Platform.image, Platform.size, self.coords)
 
 
-def random_platform_coords():
-    random.randint(0, 1)
+def random_coords_platform():
+    return Platform(Pair(
+        random.randint(Platform.min_x, Platform.max_x),
+        random.randint(Platform.min_y, Platform.max_y)
+    ))
+
+
+def bottom_floor_platforms():
+    return [
+        Platform(Pair(-half_display_size.x + i * Platform.size.x, -half_display_size.y - Platform.half_size.y))
+        for i in range(int(1.5 * display_size.x) // Platform.size.x)
+    ]
+
+
+def n_random_platforms(n):
+    return [random_coords_platform() for _ in range(n)]
+
+
+class Platforms:
+    current_count = random.randint(8, 12)
+    platforms = bottom_floor_platforms() + n_random_platforms(current_count)
+
+    @staticmethod
+    def render():
+        Platforms.filter_and_add_new()
+        for platform in Platforms.platforms:
+            platform.render()
+
+    @staticmethod
+    def filter_and_add_new():
+        filtered_platforms = []
+        # Filtering
+        for platform in Platforms.platforms:
+            if platform.coords.y >= -display_size.y:
+                filtered_platforms.append(platform)
+        # Adding new
+        new_platforms_count = Platforms.current_count - len(filtered_platforms)
+        Platforms.platforms = filtered_platforms + n_random_platforms(new_platforms_count)
